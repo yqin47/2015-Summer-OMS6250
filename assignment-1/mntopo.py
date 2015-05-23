abb4e4af0b81ccb2f3b064f3ad3726e48f733630
Dave Lillethun <davel@cc.gatech.edu>
2015-05-23 17:51:58 -0400

    Reverted my changes to this file that were accidentally committedd earlier

--- assignment-1/mntopo.py
+++ assignment-1/mntopo.py
@@ -14,23 +14,16 @@ class MNTopo(Topo):
 
         # Host and link configuration
         hostConfig = {'cpu': cpu}
-        linkConfig = {'bw': 50, 'delay': '10ms', 'loss': 0,
+        linkConfig = {'bw': 10, 'delay': '1ms', 'loss': 0,
                    'max_queue_size': max_queue_size }
 
         # Hosts and switches
         s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-
         sender = self.addHost('sender', **hostConfig)
         receiver = self.addHost('receiver', **hostConfig)
 
-	# Wire switches
-	self.addLink(s1, s2, **linkConfig)
-	self.addLink(s2, s3, **linkConfig)
-
         # Wire receiver
-        self.addLink(receiver, s3, **linkConfig)
+        self.addLink(receiver, s1, **linkConfig)
 
         # Wire sender
         self.addLink(sender, s1, **linkConfig)
